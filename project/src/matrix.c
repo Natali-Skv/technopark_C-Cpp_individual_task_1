f2bf31c6204102830d23d529ec70e6d20b0dd90c
Natali-Skv <nat-s.skv@mail.ru>
2021-10-10 22:55:42 +0300

    Task and tests have been done.

---
+++ project/src/matrix.c
@@ -0,0 +1,137 @@
+// Copyright 2021 nat-s.skv@mail.ru
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <matrix.h>
+
+#define WIDTH_OF_FIELD_FOR_ELEM "12"
+
+int **calloc_mtrx(size_t rows, size_t cols) {
+    if (!rows || !cols) {
+        fprintf(stderr, __FILE__ ": attempt to allocate 0 bytes\n");
+        return NULL;
+    }
+    int **mtrx = calloc(rows, sizeof(int *));
+    if (!mtrx) {
+        perror(__FILE__ ": calloc() failed");
+        return NULL;
+    }
+    for (size_t row = 0; row < rows; ++row) {
+        mtrx[row] = calloc(cols, sizeof(int));
+        if (!mtrx[row]) {
+            perror(__FILE__ ": calloc() failed");
+            free_mtrx(mtrx, row);
+            return NULL;
+        }
+    }
+    return mtrx;
+}
+
+void free_mtrx(int **mtrx, size_t rows) {
+    if (!mtrx) {
+        return;
+    }
+    for (size_t row = 0; row < rows; ++row) {
+        free(mtrx[row]);
+    }
+    free(mtrx);
+}
+
+int fscanf_mtrx_sizes(FILE *fin, Matrix_t *mtrx) {
+    if (!fin || !mtrx) {
+        fprintf(stderr, __FILE__ ": NULL transmitted as argument to fscanf_mtrx()\n");
+        return 1;
+    }
+    errno = 0;
+    if (fscanf(fin, "%zu%zu", &mtrx->rows, &mtrx->cols) != 2) {
+        perror(__FILE__ ": reading sizes of matrix failed");
+        return 1;
+    }
+    if (errno == ERANGE) {
+        perror(__FILE__ ": reading of matrix sizes failed: size of matrix is too large");
+        return 1;
+    }
+    return 0;
+}
+
+int fscanf_mtrx(FILE *fin, Matrix_t *mtrx) {
+    if (!fin || !mtrx) {
+        fprintf(stderr, __FILE__ ": NULL transmitted as argument\n");
+        return 1;
+    }
+    if (!mtrx->elems) {
+        fprintf(stderr, __FILE__ ": matrix is empty\n");
+        return 1;
+    }
+    for (size_t row = 0u; row < mtrx->rows; ++row) {
+        for (size_t col = 0u; col < mtrx->cols; ++col) {
+            if (fscanf(fin, "%d", &mtrx->elems[row][col]) != 1) {
+                perror(__FILE__ ": reading of matrix element  failed");
+                return 1;
+            }
+            if (errno == ERANGE) {
+                perror(__FILE__ ": reading of matrix element failed");
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+
+int fprintf_mtrx(FILE *fout, const Matrix_t *mtrx) {
+    if (!fout || !mtrx) {
+        fprintf(stderr, __FILE__ ": NULL transmitted as argument to fscanf_mtrx()\n");
+        return 1;
+    }
+    fprintf(fout, "%zu %zu\n", mtrx->rows, mtrx->cols);
+    for (size_t row = 0u; row < mtrx->rows; ++row) {
+        for (size_t col = 0u; col < mtrx->cols; ++col) {
+            fprintf(fout, "%" WIDTH_OF_FIELD_FOR_ELEM "d", mtrx->elems[row][col]);
+        }
+        fprintf(fout, "\n");
+    }
+    return 0;
+}
+
+int multiply_matrices(const Matrix_t *lhs_mtrx, const Matrix_t *rhs_mtrx, Matrix_t *product_mtrx) {
+    if (!lhs_mtrx || !rhs_mtrx || !product_mtrx) {
+        fprintf(stderr, __FILE__ ": NULL transmitted as argument to fscanf_mtrx()\n");
+        return 1;
+    }
+    if (lhs_mtrx == product_mtrx || rhs_mtrx == product_mtrx) {
+        fprintf(stderr, __FILE__ ": product-matrix pointer can not be the same as multiplier-matrix pointer\n");
+        return 1;
+    }
+    if (lhs_mtrx->cols != rhs_mtrx->rows) {
+        fprintf(stderr, __FILE__ ": multiplication is not defined for matrices lhs_mtrx->cols != rhs_mtrx->rows\n");
+        return 1;
+    }
+
+    if (!lhs_mtrx->elems || !rhs_mtrx->elems) {
+        fprintf(stderr, __FILE__ ": multiplier-matrices can not be empty\n");
+        return 1;
+    }
+
+    if (product_mtrx->elems) {
+        fprintf(stderr, __FILE__ ": product-matrix must be empty\n");
+        return 1;
+    }
+    product_mtrx->rows = lhs_mtrx->rows;
+    product_mtrx->cols = rhs_mtrx->cols;
+    product_mtrx->elems = calloc_mtrx(product_mtrx->rows, product_mtrx->cols);
+    if (!product_mtrx->elems) {
+        fprintf(stderr, __FILE__ ": malloc_mtrx() for matrix-product  failed\n");
+        product_mtrx->cols = 0u;
+        product_mtrx->rows = 0u;
+        return 1;
+    }
+    for (size_t row = 0u; row < lhs_mtrx->rows; ++row) {
+        for (size_t col = 0u; col < rhs_mtrx->cols; ++col) {
+            for (size_t i = 0u; i < lhs_mtrx->cols; ++i) {
+                product_mtrx->elems[row][col] += lhs_mtrx->elems[row][i] * rhs_mtrx->elems[i][col];
+            }
+        }
+    }
+    return 0;
+}
---
+++ project/tests/test_multiply_matrices.cpp
@@ -0,0 +1,107 @@
+// Copyright 2021 nat-s.skv@mail.ru
+extern "C" {
+#include <matrix.h>
+}
+
+#include <gtest/gtest.h>
+#include <cstring>
+#include <fstream>
+
+class FixtureMultiply : public ::testing::Test {
+protected:
+    FILE *fin = nullptr;
+    FILE *fout = nullptr;
+    FILE *foutExp = nullptr;
+    static const int bufSize = 100;
+
+    void SetUp(const char *finPath, const char *foutPath, const char *foutExpPath) {
+        fin = fopen(finPath, "r");
+        fout = fopen(foutPath, "w+");
+        foutExp = fopen(foutExpPath, "r");
+        ASSERT_TRUE(fin && fout && foutExp);
+        Matrix_t mtrx1 = {0u, 0u, NULL};
+        EXPECT_EQ(fscanf_mtrx_sizes(fin, &mtrx1), 0);
+        mtrx1.elems = calloc_mtrx(mtrx1.rows, mtrx1.cols);
+        ASSERT_TRUE(mtrx1.elems || !mtrx1.rows || !mtrx1.cols);
+        EXPECT_EQ(fscanf_mtrx(fin, &mtrx1), 0);
+
+        Matrix_t mtrx2 = {0u, 0u, NULL};
+        EXPECT_EQ(fscanf_mtrx_sizes(fin, &mtrx2), 0);
+        mtrx2.elems = calloc_mtrx(mtrx2.rows, mtrx2.cols);
+        ASSERT_TRUE(mtrx2.elems || !mtrx2.rows || !mtrx2.cols);
+        EXPECT_EQ(fscanf_mtrx(fin, &mtrx2), 0);
+
+        Matrix_t mtrx_product = {0u, 0u, NULL};
+        multiply_matrices(&mtrx1, &mtrx2, &mtrx_product);
+        EXPECT_EQ(fprintf_mtrx(fout, &mtrx_product), 0);
+
+        fseek(fout, 0, SEEK_SET);
+        CompareFiles(fout, foutExp);
+        free_mtrx(mtrx1.elems, mtrx1.rows);
+        free_mtrx(mtrx2.elems, mtrx2.rows);
+        free_mtrx(mtrx_product.elems, mtrx_product.rows);
+    }
+
+    void CompareFiles(FILE *f1, FILE *f2) {
+        char bufFin[bufSize];
+        char bufFout[bufSize];
+        while (!feof(f1) && !feof(f2)) {
+            fgets(bufFin, bufSize, f1);
+            fgets(bufFout, bufSize, f2);
+            EXPECT_EQ(std::memcmp(bufFin, bufFout, strlen(bufFin)), 0);
+        }
+        EXPECT_TRUE(feof(f1) && feof(f2));
+    }
+
+    void TearDown() {
+        EXPECT_EQ(fclose(fin), 0);
+        EXPECT_EQ(fclose(fout), 0);
+        EXPECT_EQ(fclose(foutExp), 0);
+    }
+};
+
+TEST_F(FixtureMultiply, MULTIPLY_2X2_X_2X4) {
+    FixtureMultiply::SetUp(PROJECT_PATH "tests/input_emulator/multiply_matrices/2x2_x_2x4/fin",
+                           PROJECT_PATH "tests/input_emulator/multiply_matrices/2x2_x_2x4/fout",
+                           PROJECT_PATH "tests/input_emulator/multiply_matrices/2x2_x_2x4/fexp");
+}
+
+TEST_F(FixtureMultiply, MULTIPLY_2X2_X_2X7) {
+    FixtureMultiply::SetUp(PROJECT_PATH "tests/input_emulator/multiply_matrices/2x2_x_2x7/fin",
+                           PROJECT_PATH "tests/input_emulator/multiply_matrices/2x2_x_2x7/fout",
+                           PROJECT_PATH "tests/input_emulator/multiply_matrices/2x2_x_2x7/fexp");
+}
+
+TEST_F(FixtureMultiply, MULTIPLY_5X6_X_6X7) {
+    FixtureMultiply::SetUp(PROJECT_PATH "tests/input_emulator/multiply_matrices/5x6_x_6x7/fin",
+                           PROJECT_PATH "tests/input_emulator/multiply_matrices/5x6_x_6x7/fout",
+                           PROJECT_PATH "tests/input_emulator/multiply_matrices/5x6_x_6x7/fexp");
+}
+
+
+TEST(MULTIPLY_MATRICES, MULTIPLY_INVALID_ARGS) {
+    Matrix_t mtrx_2x2 = {2u, 2u, NULL};
+    Matrix_t mtrx_2x1 = {2u, 1u, NULL};
+    Matrix_t mtrx_1x2 = {1u, 2u, NULL};
+    ASSERT_TRUE(mtrx_2x2.elems = calloc_mtrx(mtrx_2x2.rows, mtrx_2x2.cols));
+    ASSERT_TRUE(mtrx_2x1.elems = calloc_mtrx(mtrx_2x1.rows, mtrx_2x1.cols));
+    ASSERT_TRUE(mtrx_1x2.elems = calloc_mtrx(mtrx_1x2.rows, mtrx_1x2.cols));
+    Matrix_t mtrx_2x2_empty = {2u, 2u, NULL};
+    Matrix_t mtrx_2x1_empty = {2u, 1u, NULL};
+    Matrix_t mtrx_1x2_empty = {1u, 2u, NULL};
+
+    EXPECT_NE(multiply_matrices(NULL, &mtrx_2x2, &mtrx_1x2_empty), 0);
+    EXPECT_NE(multiply_matrices(&mtrx_2x2, NULL, &mtrx_2x1_empty), 0);
+    EXPECT_NE(multiply_matrices(&mtrx_2x2, &mtrx_2x1, NULL), 0);
+
+    EXPECT_NE(multiply_matrices(&mtrx_2x1_empty, &mtrx_1x2, &mtrx_2x2_empty), 0);
+    EXPECT_NE(multiply_matrices(&mtrx_2x2, &mtrx_2x1_empty, &mtrx_2x1_empty), 0);
+
+    // multiplication is not defined for matrices lhs_mtrx->cols != rhs_mtrx->rows
+    EXPECT_NE(multiply_matrices(&mtrx_2x2, &mtrx_1x2, &mtrx_2x2_empty), 0);
+    EXPECT_NE(multiply_matrices(&mtrx_2x1, &mtrx_2x2, &mtrx_2x2_empty), 0);
+
+    free_mtrx(mtrx_2x2.elems, mtrx_2x2.rows);
+    free_mtrx(mtrx_1x2.elems, mtrx_1x2.rows);
+    free_mtrx(mtrx_2x1.elems, mtrx_2x1.rows);
+}
